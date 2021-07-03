91215876b21ad4dafeaf3488e77cb5daac961629
dangthanhtri <dangthanhtrispkt@gmail.com>
2021-07-03 23:46:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "environment" }, audio: false };
+var constraints = { video: { facingMode: { exact: "environment" } }, audio: false };
 var track = null;
 
 // Define constants
@@ -17,7 +17,8 @@ function cameraStart() {
             cameraView.srcObject = stream;
         })
         .catch(function(error) {
-            console.error("Oops. Something is broken.", error);
+          alert("Oops. Something is broken.", error);
+            //console.error("Oops. Something is broken.", error);
         });
 }
 
